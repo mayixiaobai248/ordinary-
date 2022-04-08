a8d093a06ede93ac96ca07b4928cd0a61357a8f6
mayixiaobai248 <73387592+mayixiaobai248@users.noreply.github.com>
2022-04-08 09:43:24 +0800

    Add files via upload

---
+++ 网页爬虫2.0.py
@@ -0,0 +1,17 @@
+import urllib.request
+
+num = 0
+for line in open('name.txt'):
+    print(line)
+    num = num + 1
+    try:
+        head = { "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"}
+        request = urllib.request.Request(line, headers=head)
+        data = urllib.request.urlopen(request).read().decode()
+        print(data)
+
+        file1 = open("网页"+str(num)+".html", 'w', encoding='UTF-8')
+        file1.write(data)
+    except:
+        continue
+
